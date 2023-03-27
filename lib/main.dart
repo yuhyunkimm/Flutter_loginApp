25e81193497d153a005dd6cdde69cb2fcd145d7d
YouHyunKim <yuhyunk7@gmail.com>
2023-03-27 09:51:48 +0900

    main scaffold / size 지정

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/login",
+      routes: {
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
+    );
+  }
+}
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.yellow,),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(color: Colors.red,),
+    );
+  }
+}
---
+++ lib/size.dart
@@ -0,0 +1,4 @@
+const  s_gap = 5.0;
+const  m_gap = 10.0;
+const  l_gap = 20.0;
+const  xl_gap = 100.0;

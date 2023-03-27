b8938097ca244664332bd38ad27a801332102551
YouHyunKim <yuhyunk7@gmail.com>
2023-03-27 14:07:33 +0900

    home page 추가 완료

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.red,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(height: 200,),
+            Logo("Care Soft"),
+            SizedBox(height: 50,),
+            TextButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: Text("Get Started"),
+            ),
+          ],
+        ),
       ),
     );
   }

8aa9639daa4a3982ab873ab4f1f1e720dde60a96
YouHyunKim <yuhyunk7@gmail.com>
2023-03-27 10:55:15 +0900

    components -logo 생성

---
+++ lib/components/logo.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Logo extends StatelessWidget {
+
+  final title;
+  // final String title; final 뒤 생략 가능
+  // Logo({required this.title, Key? key}) : super(key: key);
+  Logo( this.title,{Key? key}) : super(key: key); //시그니처는 하나만 받을 때 앞에 넣어서 사용
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset("assets/logo.svg", height: 70, width: 70,),
+        Text(
+          title,
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/components/logo.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +27,13 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(color: Colors.yellow,),
+      body: ListView(
+        children: [
+          Logo("Login"),
+          TextFormField(),
+
+        ],
+      ),
     );
   }
 }
